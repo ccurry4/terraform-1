41da2b8b6feaaa55b553adcf0849136e74737478
ccurry4 <ccurry4@gmail.com>
2020-02-20 18:23:52 -0500

    Update main.tf

--- exercise_3/main.tf
+++ exercise_3/main.tf
@@ -16,6 +16,7 @@ resource "aws_s3_bucket" "tf-root-module-bucket" {
   bucket = "${var.s3_bucket_name}"
   acl = "private"
   region = "${var.s3_bucket_region}"
+  
   versioning {
     enabled = true
   }
