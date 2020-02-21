157c160753b607f1c6ef785279aec45786f9af5e
ccurry4 <ccurry4@gmail.com>
2020-02-21 11:19:08 -0500

    Update main.tf

--- exercise_3/main.tf
+++ exercise_3/main.tf
@@ -1,18 +1,18 @@
-provider 'aws' {
+provider "aws" {
   access_key = '${var.access_key}'
   secret_key = '${var.secret_key}'
   region = '${var.region}'
 }
 
 terraform {
-   backend 's3' {
+   backend "s3" {
    bucket = 'cc-tf-bucket-training-exercise-3'
    key = 'terraform.tfstate'
    region = 'us-east-1'
   }
 }
 
-resource 'aws_s3_bucket' 'tf-root-module-bucket' {
+resource "aws_s3_bucket" "tf-root-module-bucket" {
   bucket = '${var.s3_bucket_name}'
   acl = 'private'
   region = '${var.s3_bucket_region}'
